194247abe56e852fe7828838bce38cd6bd598182
Faishalzaky <faishalzaky2000@gmail.com>
2026-03-06 00:20:37 +0700

    add file day4

---
+++ JavaScript_day4/script.js
@@ -0,0 +1,9 @@
+let role = "admin";
+
+if (role === "admin") {
+    console.log("Akses penuh");
+} else if (role === "editor") {
+    console.log("Akses edit");
+} else {
+    console.log("Akses terbatas");
+}
